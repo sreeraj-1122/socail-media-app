41fa11642711fea9645ffae8c41b9b2387a4841b
sreeraj <sreeraj2122@gmail.com>
2024-08-25 13:34:41 +0530

    create mobile navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,67 +1,114 @@
-import React from "react";
+import React, { useState } from "react";
 import { GoSearch } from "react-icons/go";
-import { FaRegBell } from "react-icons/fa";
+import { FaRegBell, FaBars, FaTimes } from "react-icons/fa";
+import { MdOutlineLightMode, MdOutlineDarkMode } from "react-icons/md";
 import { useStore } from "../../context/StoreContextProvider";
+import { RiHomeLine, RiLogoutBoxRLine, RiUserLine } from "react-icons/ri";
 
 const Navbar = () => {
   const { isDarkMode, setIsDarkMode } = useStore();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
-    <nav className="flex justify-between items-center px-8 py-3 shadow-lg dark:bg-custom-bg">
-      {/* Logo */}
-      <div className="text-3xl font-bold text-blue-600 cursor-pointer">
-        Insta<span className="text-orange-600">Stream</span>
-      </div>
+    <nav className="shadow-lg dark:bg-custom-bg">
+      {/* Desktop Navbar */}
+      <div className="hidden md:flex justify-between items-center px-8 py-3">
+        {/* Logo */}
+        <div className="text-3xl font-bold text-blue-600 cursor-pointer">
+          Insta<span className="text-orange-600">Stream</span>
+        </div>
 
-      {/* Search Bar */}
-      <div className="flex items-center w-1/3 dark:border-gray-500 rounded-md border border-gray-300 ">
-        <input
-          type="text"
-          placeholder="Search..."
-          className="p-2 border-none outline-none w-full dark:text-white dark:bg-[#212b36] rounded-s-md"
-        />
-        <button className="bg-blue-800 h-full py-2 px-4 text-white rounded-e-md cursor-pointer">
-          Search
-        </button>
-      </div>
-
-      {/* Theme Switcher and Notifications */}
-      <div className="flex items-center space-x-4">
-        {/* Theme Switcher */}
-        <label
-          className="themeSwitcherTwo relative inline-flex cursor-pointer select-none items-center"
-          htmlFor="themeSwitch"
-        >
+        {/* Search Bar */}
+        <div className="flex items-center w-1/3 dark:border-gray-500 rounded-md border border-gray-300 shadow">
           <input
-            id="themeSwitch"
-            type="checkbox"
-            checked={isDarkMode}
-            onChange={() => setIsDarkMode(!isDarkMode)}
-            className="sr-only"
-            aria-label="Toggle theme"
+            type="text"
+            placeholder="Search..."
+            className="ps-2 py-2 border-none outline-none w-full dark:text-white dark:bg-[#212b36] rounded-s-md"
           />
-          <span
-            className={`slider mx-4 flex h-8 w-[60px] items-center rounded-full p-1 duration-200 ${
-              isDarkMode ? "bg-[#25313d]" : "bg-[#c0c0c3]"
-            }`}
-          >
-            <span
-              className={`dot h-6 w-6 rounded-full bg-white duration-200 ${
-                isDarkMode ? "translate-x-[28px]" : ""
-              }`}
-            ></span>
+          <span className="bg-blue-800 h-full py-3 px-5 text-white rounded-e-md cursor-pointer shadow">
+            <GoSearch className="text-xl font-bold" />
           </span>
-        </label>
+        </div>
 
-        {/* Notification Icon */}
-        <span className="text-xl text-gray-600 dark:text-gray-200">
-          <FaRegBell aria-label="Notifications" />
+        {/* Theme Switcher and Notifications */}
+        <div className="flex items-center space-x-4 gap-1">
+          {/* Theme Switcher */}
+          <span onClick={() => setIsDarkMode(!isDarkMode)}>
+            {isDarkMode ? (
+              <MdOutlineLightMode className="text-2xl" />
+            ) : (
+              <MdOutlineDarkMode className="text-2xl" />
+            )}
+          </span>
+
+          {/* Notification Icon */}
+          <span className="text-xl text-gray-600 dark:text-gray-200">
+            <FaRegBell aria-label="Notifications" />
+          </span>
+
+          {/* Logout Button */}
+          <button className="border-red-400 border rounded-md inline-flex items-center justify-center py-2 px-3 text-center text-base font-medium hover:bg-[#d63d0f] text-white flext gap-2 bg-orange-600">
+            Logout
+            <RiLogoutBoxRLine />
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Navbar */}
+      <div className="flex md:hidden justify-between items-center px-3 gap-3 py-3 ">
+        {/* Logo */}
+        <div className="text-2xl font-bold text-blue-600 cursor-pointer">
+          Insta<span className="text-orange-600">Stream</span>
+        </div>
+        <div className="flex items-center w-[40%] dark:border-gray-500 rounded-md border border-gray-300 shadow ">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="ps-2 py-1 border-none outline-none w-full dark:text-white dark:bg-[#212b36] rounded-s-md"
+          />
+          <span className="bg-blue-800 h-full py-2 px-3 text-white rounded-e-md cursor-pointer shadow">
+            <GoSearch className="text-xl font-bold" />
+          </span>
+        </div>
+
+        {/* Theme Switcher */}
+        <span onClick={() => setIsDarkMode(!isDarkMode)}>
+          {isDarkMode ? (
+            <MdOutlineLightMode className="text-2xl" />
+          ) : (
+            <MdOutlineDarkMode className="text-2xl" />
+          )}
         </span>
 
-        {/* Logout Button */}
-        <button className="border-red-400 border rounded-md inline-flex items-center justify-center py-2 px-7 text-center text-base font-medium  hover:bg-[#de2828]">
-          Logout
+        {/* Hamburger Menu */}
+        <button
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="text-2xl text-gray-600 dark:text-gray-200"
+        >
+          {isMenuOpen ? <FaTimes /> : <FaBars />}
         </button>
+      </div>
+
+      {/* Mobile Menu */}
+      <div
+        className={`fixed top-16 right-0 h-full w-[40%]  bg-[#e9e9e9] dark:bg-[#121212] shadow-md transform transition-transform duration-300 ease-in-out ${
+          isMenuOpen ? "translate-x-0" : "translate-x-full"
+        }`}
+      >
+        <ul className="flex flex-col items-center gap-2 pt-3">
+          <li className=" items-center justify-center text-center text-base font-medium hover:text-white flex gap-2 hover:bg-slate-700 w-full py-2">
+            Home
+            <RiHomeLine /> {/* Replace with appropriate icon */}
+          </li>
+          <li className=" items-center justify-center text-center text-base font-medium hover:text-white flex gap-2 hover:bg-slate-600 w-full py-2">
+            Profile
+            <RiUserLine /> {/* Replace with appropriate icon */}
+          </li>
+          <li className=" items-center justify-center text-center text-base font-medium hover:text-white flex gap-2 hover:bg-slate-600 w-full py-2">
+            Logout
+            <RiLogoutBoxRLine />
+          </li>
+        </ul>
       </div>
     </nav>
   );
